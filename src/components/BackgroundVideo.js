ffbca149f50f876a58e8aed76ee6aa676cc8b15c
Undriling <manashbaruah219@gmail.com>
2025-04-22 01:39:25 +0530

    Heading Video Play

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import useMovieTrailer from '../hooks/useMovieTrailer'
+import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
-const BackgroundVideo = ({movieId}) => {
-    const trailerVideo = useSelector((store) => store.movies?.movieTrailer)
-   
-    useMovieTrailer(movieId);
+const BackgroundVideo = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.movieTrailer);
+
+  useMovieTrailer(movieId);
 
   return (
-    <div className='absolute mt-10 md:mt-0'>
-        <iframe className='w-screen aspect-video absolute' src={"https://www.youtube.com/embed/"+ trailerVideo?.key + "?playlist="+ trailerVideo?.key + "&autoplay=1&mute=1&loop=1&controls=0&rel=0&showinfo=0&modestbranding=1&iv_load_policy=3"} 
-            title="YouTube video player"  allow="autoplay; encrypted-media; web-share" 
-            referrerPolicy="strict-origin-when-cross-origin">
-            </iframe>
+    <div className="absolute mt-10 md:mt-0">
+      <iframe
+        className="w-screen aspect-video absolute"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?playlist=" +
+          trailerVideo?.key +
+          "&autoplay=1&mute=1&loop=1&controls=0&rel=0&showinfo=0&modestbranding=1&iv_load_policy=3"
+        }
+        title="YouTube video player"
+        allow="autoplay; encrypted-media; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"></iframe>
     </div>
-  )
-}
+  );
+};
 
 export default BackgroundVideo;
--- src/components/BgVideoTitle.js
+++ src/components/BgVideoTitle.js
@@ -1,29 +1,63 @@
-import React, { useState } from 'react'
-import { MoviePlayer_URL } from '../constants';
+import React, { useState } from "react";
+import { MoviePlayer_URL } from "../constants";
+import { BsInfoCircle } from "react-icons/bs";
 
-const BgVideoTitle = ({title, description, id}) => {
-    const [showDescription, setShowDescription] = useState(false)
-
-    // const goToReviewPage = () => {
-    //     window.open(MoviePage_URLPi + id + MoviePage_URLPii, '_blank')
-    //   };
-
-    const handleClick = () => {
-        window.open(MoviePlayer_URL + id, '_blank')
-      };
+const BgVideoTitle = ({ title, description, id }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showDescription, setShowDescription] = useState(false);
 
   return (
-    <div className='absolute text-white font-serif bg-gradient-to-b from-black w-screen aspect-video md:mt-0 mt-[32px]'>
-        <div className='my-[17%] mx-4 md:mx-10'>
-            <h1 className='text-xl md:text-6xl font-serif font-extrabold'>{title}</h1>
-            {showDescription && <p className='md:w-1/2 text-xs md:text-lg my-1 md:my-4 '>{description}</p>}
-            <div className='flex flex-wrap md:ml-0 my-2 md:my-7'>
-                <button className='bg-white text-black w-20 md:w-36 h-6 md:h-10 rounded-md hover:bg-opacity-80' onClick={handleClick}>▶ Play</button>
-                <button className=' bg-gray-500 text-black w-20 md:w-36 h-6 md:h-10 rounded-md mx-3 hover:bg-opacity-80' onClick={() => setShowDescription(!showDescription)}> More Info</button>
+    <div className="absolute text-white font-serif bg-gradient-to-b from-black w-screen aspect-video md:mt-0 mt-[32px]">
+      <div className="my-[17%] mx-4 md:mx-10">
+        <h1 className="text-xl md:text-6xl font-serif font-extrabold">
+          {title}
+        </h1>
+
+        {!isPlaying && (
+          <>
+            {showDescription && (
+              <p className="md:w-1/2 text-xs md:text-lg my-1 md:my-4">
+                {description}
+              </p>
+            )}
+
+            <div className="flex flex-wrap md:ml-0 my-2 md:my-7">
+              <button
+                className="bg-white text-black w-20 md:w-36 h-6 md:h-10 rounded-md hover:bg-opacity-80"
+                onClick={() => setIsPlaying(true)}>
+                ▶ Play
+              </button>
+
+              <button
+                className="bg-gray-500 text-black flex items-center justify-center w-20 md:w-36 h-6 md:h-10 rounded-md mx-3 hover:bg-opacity-80"
+                onClick={() => setShowDescription(!showDescription)}>
+                <BsInfoCircle /> More Info
+              </button>
             </div>
+          </>
+        )}
+      </div>
+
+      {isPlaying && (
+        <div className="fixed inset-0 bg-black bg-opacity-95 z-50 flex justify-center items-center">
+          <div className="relative w-full h-screen">
+            <button
+              className="absolute top-4 right-6 text-white text-3xl z-10"
+              onClick={() => setIsPlaying(false)}>
+              ✖
+            </button>
+
+            <iframe
+              src={MoviePlayer_URL + id}
+              allowFullScreen
+              className="w-full h-full rounded-md"
+              title="Movie Player"
+            />
+          </div>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default BgVideoTitle;
